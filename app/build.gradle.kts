a816affaf07aec81b4c90fa89aea74c34e1fbfdb
Josip Tišljar Mataušić <josip.tismat@gmail.com>
2026-03-08 20:20:02 +0100

    [chore] Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.zutiradio.callmachine"
         minSdk = 29
         targetSdk = 36
-        versionCode = 27
-        versionName = "0.0.27"
+        versionCode = 28
+        versionName = "0.0.28"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
